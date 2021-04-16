94504c57045f9cf09b32353bd77cce9d31c0e5cd
santi07a <santi07a@gmail.com>
2021-04-16 11:41:26 +0200

    se envian mails cuando se registran usa

--- rutas/usuarios.js
+++ rutas/usuarios.js
@@ -2,9 +2,19 @@ const express = require("express");
 const { checkSchema } = require("express-validator");
 const debug = require("debug")("incidencias:usuarios");
 const md5 = require("md5");
+const nodemailer = require("nodemailer");
 const { getUsuarios, getUsuario, crearUsuario } = require("../controladores/usuarios");
 const { badRequestError } = require("../errores/errores");
 const { getUsuarioSchemaCompleto, getUsuarioSchemaParcial } = require("../schemas/usuarioSchema");
+
+const transport = nodemailer.createTransport({
+  host: "smtp-mail.outlook.com",
+  port: 587,
+  auth: {
+    user: "ciutadaverd@outlook.es",
+    pass: "somdevbmns202!"
+  }
+});
 
 const router = express.Router();
 
@@ -39,7 +49,20 @@ router.post("/usuario", checkSchema(getUsuarioSchemaCompleto),
     if (error) {
       next(error);
     } else {
-      res.status(201).json({ id: usuario.id });
+      const mensaje = {
+        from: "ciutadaverd@outlook.es",
+        to: usuario.email,
+        subject: "confirmación registro en Ciutadá Verd",
+        html: "<h1>Su registro ha sido confirmado</h1><br/><p>Muchas gracias por registrarte con nosotros. Hoy eres un ciudadano mas comprometido con el ambiente y con la ciudad</p>"
+      };
+      transport.sendMail(mensaje, (err, info) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(info);
+        }
+        res.status(201).json({ id: usuario.id });
+      });
     }
   });
 module.exports = router;
