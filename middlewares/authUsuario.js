7194765c8fef8dd97be42a3d733d3d50ab4e8d74
santi07a <santi07a@gmail.com>
2021-04-21 17:08:34 +0200

    creado auth Usuario

---
+++ middlewares/authUsuario.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const { generaError } = require("../utils/errores");
+
+const authUsuario = (req, res, next) => {
+  if (!req.header("Authorization")) {
+    return next(generaError("Falta el token", 403));
+  }
+  const token = req.header("Authorization") ? req.header("Authorization").split(" ")[1] : "";
+  try {
+    const infoUsuario = jwt.verify(token, process.env.JWT_SECRET);
+    req.idUsuario = infoUsuario.id;
+    next();
+  } catch (err) {
+    return next(generaError(err.message, 403));
+  }
+};
+
+module.exports = authUsuario;
